1613fa5c7ee947605462442bb3f3237f2798313e
Augustin Delecluse <augustin.delecluse@hotmail.com>
2024-03-11 18:04:04 +0100

    reduced memory footprint of BestManual selector

--- solver/src/main/java/org/chocosolver/solver/search/strategy/selectors/values/IntDomainBestManual.java
+++ solver/src/main/java/org/chocosolver/solver/search/strategy/selectors/values/IntDomainBestManual.java
@@ -9,12 +9,10 @@
  */
 package org.chocosolver.solver.search.strategy.selectors.values;
 
-import org.chocosolver.solver.Cause;
-import org.chocosolver.solver.ConstraintNetwork;
-import org.chocosolver.solver.Model;
-import org.chocosolver.solver.ResolutionPolicy;
+import org.chocosolver.solver.*;
 import org.chocosolver.solver.constraints.Propagator;
 import org.chocosolver.solver.exception.ContradictionException;
+import org.chocosolver.solver.propagation.PropagationEngine;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.Variable;
 import org.chocosolver.solver.variables.events.PropagatorEventType;
@@ -25,8 +23,11 @@ import java.util.function.Function;
 
 public class IntDomainBestManual extends IntDomainBestPruning {
 
-    protected Map<Variable, List<Set<Propagator<?>>>> constraintsOnShortestPath = new HashMap<>();
-    private Map<IView<?>, Variable> viewToClosestVariableToObjective;
+    private CompactConstraintNetwork network = null;
+
+    HashSet<Variable> currentVariables = new HashSet<>();
+    HashSet<Variable> nextVariables = new HashSet<>();
+    HashSet<Propagator<?>> propagated = new HashSet<>();
 
     public IntDomainBestManual() {
         super();
@@ -37,28 +38,11 @@ public class IntDomainBestManual extends IntDomainBestPruning {
     }
 
     private void createConstraintGraph(Model model) {
-        ConstraintNetwork constraintGraph = new ConstraintNetwork(model);
-        viewToClosestVariableToObjective = constraintGraph.getViewToClosestVariableToObjective();
-        for (Variable var: constraintGraph.variables()) {
-            constraintsOnShortestPath.put(var, new ArrayList<>());
-            int distToObjective = constraintGraph.hopToObjective(var);
-            int i = 0;
-            for (int depth = distToObjective - 1 ; depth >= 0  ; depth--) {
-                constraintsOnShortestPath.get(var).add(new HashSet<>());
-                for (Propagator<?> propagator: constraintGraph.constraintsAtDistanceFromObjective(var, depth)) {
-                    constraintsOnShortestPath.get(var).get(i).add(propagator);
-                }
-                i++;
-            }
-        }
+        network = new CompactConstraintNetwork(model);
     }
 
     private boolean isConstraintGraphCreated() {
-        return !constraintsOnShortestPath.isEmpty();
-    }
-
-    private boolean isView(Variable variable) {
-        return (variable.getTypeAndKind() & Variable.VIEW) !=0;
+        return network != null;
     }
 
     @Override
@@ -72,20 +56,40 @@ public class IntDomainBestManual extends IntDomainBestPruning {
 
     @Override
     protected int boundWithPropagate(IntVar var, int val) throws ContradictionException {
-        IntVar originalVar = isView(var) ? (IntVar) viewToClosestVariableToObjective.get((IView<?>) var) : var;
+        IntVar originalVar = (IntVar) network.getMostRelevantVar(var);
         Model model = var.getModel();
         dop.apply(var, val, Cause.Null);
-        int distToObjective = constraintsOnShortestPath.get(originalVar).size();
-        for (int level = 0; level < distToObjective ; level++) {
-            for (Propagator<?> propagator: constraintsOnShortestPath.get(originalVar).get(level)) {
-                try {
-                    if (propagator.isActive()) {
-                        propagator.propagate(PropagatorEventType.FULL_PROPAGATION.getMask());
+        PropagationEngine engine = model.getSolver().getEngine();
+        var.schedulePropagators(engine);
+        CompactConstraintNetwork.VarNode varNode = network.getVarNode(originalVar);
+        if (varNode != null) {
+            // propagate manually the constraints until reaching the objective
+            currentVariables.clear();
+            nextVariables.clear();
+            propagated.clear();
+            currentVariables.add(originalVar);
+            while (!currentVariables.isEmpty()) {
+                for (Variable child : currentVariables) {
+                    Variable childV = network.getMostRelevantVar(child);
+                    for (CompactConstraintNetwork.DirectedPropagatorEdge edge : network.getVarNode(childV).parents) {
+                        Propagator<?> propagator = edge.propagator;
+                        if (propagator.isActive() && propagator.isScheduled() && !propagated.contains(propagator)) {
+                            try {
+                                engine.execute(propagator);
+                            } catch (ContradictionException cex) {
+                                model.getSolver().getEngine().flush();
+                                throw cex;
+                            }
+                            nextVariables.add(edge.target);
+                            propagated.add(propagator);
+                        }
                     }
-                } catch (ContradictionException cex) {
-                    model.getSolver().getEngine().flush();
-                    throw cex;
                 }
+                // clear currentVariables and swap with nextVariables
+                currentVariables.clear();
+                HashSet<Variable> tmp = currentVariables;
+                currentVariables = nextVariables;
+                nextVariables = tmp;
             }
         }
         model.getSolver().getEngine().flush();
